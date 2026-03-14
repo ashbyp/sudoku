5a149e39941f200a0c8ce828828beefc64e030ea
ashbyp <ashbyp@yahoo.co.uk>
2026-03-14 22:28:16 +0000

    Simplify naked single test

--- tests/test_hints.py
+++ tests/test_hints.py
@@ -2,13 +2,19 @@ from app.core.hints import _solve_xwing_hint, get_hint
 
 
 def test_hint_prefers_singles():
-    board = [[0] * 9 for _ in range(9)]
-    board[0][1] = 2
-    board[1][0] = 3
-    board[1][1] = 4
-    # Force only one empty cell with candidate 1.
-    board[0][0] = 0
-    hint = get_hint(board)
+    solution = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [4, 5, 6, 7, 8, 9, 1, 2, 3],
+        [7, 8, 9, 1, 2, 3, 4, 5, 6],
+        [2, 3, 4, 5, 6, 7, 8, 9, 1],
+        [5, 6, 7, 8, 9, 1, 2, 3, 4],
+        [8, 9, 1, 2, 3, 4, 5, 6, 7],
+        [3, 4, 5, 6, 7, 8, 9, 1, 2],
+        [6, 7, 8, 9, 1, 2, 3, 4, 5],
+        [9, 1, 2, 3, 4, 5, 6, 7, 8],
+    ]
+    solution[0][0] = 0
+    hint = get_hint(solution)
     assert "Naked single" in hint["message"]
 
 
